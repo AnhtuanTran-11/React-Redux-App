03c174ac1e4887d3de3b614664da453e0c7c577f
Anhtuan Tran <anhtuantran.github@gmail.com>
2021-02-18 00:07:39 -0600

    MVP

--- random-quote/src/App.js
+++ random-quote/src/App.js
@@ -1,26 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import Pics from "./components/Pics";
 
 function App() {
-
-  const [dog, setDog] = useState('');
-
-  useEffect(() => { 
-    axios.get('https://random.dog/woof.json')
-    .then(res => {
-      setDog(res.data)
-      console.log(res.data)
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  }, [])
-
   return (
     <div>
-      <h1>Random dog</h1>
-      <br/>
-      <img src={dog.url} alt=""/>
+        <Pics />
     </div>
   );
 }
--- random-quote/src/index.js
+++ random-quote/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import { reducer } from "./reducers/reducer";
+import { createStore, applyMiddleware } from 'redux';
+
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
